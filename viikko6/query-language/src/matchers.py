a9a1cb9a29002460e7c6b752c1468167ede30907
Veeti Kupari <Veetikupari@gmail.com>
2025-12-08 19:04:46 +0200

    viikko 6 complete

---
+++ viikko6/query-language/src/matchers.py
@@ -0,0 +1,87 @@
+class And:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if not matcher.test(player):
+                return False
+
+        return True
+
+
+class PlaysIn:
+    def __init__(self, team):
+        self._team = team
+
+    def test(self, player):
+        return player.team == self._team
+
+
+class HasAtLeast:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value >= self._value
+
+class All:
+    def test(self, player):
+        return True
+    
+class Not:
+    def __init__(self, matcher):
+        self.matcher = matcher
+
+    def test(self, player):
+        return not self.matcher.test(player)
+    
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
+    
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+        return False
+
+class QueryBuilder:
+    def __init__(self, matchers=None):
+        if matchers is None:
+            matchers = []
+        self._matchers = matchers
+
+    def plays_in(self, team):
+        return QueryBuilder(self._matchers + [PlaysIn(team)])
+
+    def has_at_least(self, value, attr):
+        return QueryBuilder(self._matchers + [HasAtLeast(value, attr)])
+
+    def has_fewer_than(self, value, attr):
+        return QueryBuilder(self._matchers + [HasFewerThan(value, attr)])
+    
+    def one_of(self, *matchers):
+        return QueryBuilder(self._matchers + [Or(*matchers)])
+
+    def build(self):
+        if not self._matchers:
+            return All()
+        elif len(self._matchers) == 1:
+            return self._matchers[0]
+        else:
+            return And(*self._matchers)
